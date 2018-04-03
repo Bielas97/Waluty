a97d95a84ff86e34b1b586a8d59835fe6fb96563
Kuba <jakub.bielawski@wawasoft.com>
2018-04-03 22:51:39 +0200

    branch mateusza

--- src/main/java/com/waluty/waluty/WalutyApplication.java
+++ src/main/java/com/waluty/waluty/WalutyApplication.java
@@ -3,6 +3,7 @@ package com.waluty.waluty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Branch mateusza
 @SpringBootApplication
 public class WalutyApplication {
 
