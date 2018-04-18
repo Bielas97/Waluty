91724e9abdb907287488eb5a8a8e4a55d26138c2
ost96 <37448385+ost96@users.noreply.github.com>
2018-04-18 15:53:38 +0200

    Stworzenie klasy CurrencyDontroller w pakiecie controllers, na pewno czegoś brakuje, nie wiem czego

---
+++ src/main/java/com/waluty/controllers/CurrencyController.java
@@ -0,0 +1,24 @@
+package com.waluty.controllers;
+
+import com.waluty.model.Currency;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class CurrencyController{
+
+    @GetMapping("/getAll")
+    public String getCurrency(Model model){
+        model.addAttribute("currency", Currency.values());
+
+        return "getAll";
+    }
+
+
+}
