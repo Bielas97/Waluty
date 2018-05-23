7f14293c096bb19be44241dd4b98541a50bdf156
ost96 <37448385+ost96@users.noreply.github.com>
2018-05-23 21:50:45 +0200

    Stworzenie 3 testów

--- src/test/java/com/waluty/WalutyApplicationTests.java
+++ src/test/java/com/waluty/WalutyApplicationTests.java
@@ -4,13 +4,33 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.*;
+import org.springframework.ui.Model;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WalutyApplicationTests {
+	ConverterDto convDto = new ConverterDto();
+	CurrencyController control = new CurrencyController();
+	Model model = new Model();
+
+	currDto = convDto.fromCurrencyToCurrencyDto(null);
+	curr = convDto.fromCurrencyDtoToCurrency(null);
 
 	@Test
-	public void contextLoads() {
+	public void testFromCurrencyToCurrencyDto() {
+		assertNull(currDto);
+	}
+
+	@Test
+	public void testFromCurrencyDtoToCurrency() {
+		assertNull(curr);
+	}
+
+	@Test
+	public void testGetCurrency() {
+		testMessage = "getAll";
+		assertEquals(testMessage,control.getCurrency(model));
 	}
 
 }
